6f99c9df0c6576c7b7dd7a64f4fcb641c500e0ec
ahmedHarb <aharb@averroes.ai>
2025-02-15 00:55:49 +0200

    feat: Add tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
-import "./App.css";
-
-const App = () => <h2> hello Ahmed</h2>;
+const App = () => (
+  <h2 className="text-3xl bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 font-bold">
+    hello Ahmed
+  </h2>
+);
 
 export default App;
